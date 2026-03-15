8bb5ce0802ae70776c220e3964c95ebd0f91122d
phantekzy <mainilotfi@gmail.com>
2026-03-15 21:46:48 +0100

    Property Access

--- worker/src/engine.ts
+++ worker/src/engine.ts
@@ -1,1 +1,9 @@
-export class LoadEngine {}
+import axios, { AxiosInstance } from "axios";
+
+export class LoadEngine {
+  private active = false;
+  private http: AxiosInstance = axios.create({
+    timeout: 5000,
+    validateStatus: () => true,
+  });
+}
