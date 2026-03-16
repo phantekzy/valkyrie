ee3c69c181b7487d6575149f8fbd49bd81220e0d
phantekzy <mainilotfi@gmail.com>
2026-03-16 04:03:20 +0100

    fixed the Root Directory Issue and the the CommonJS/ESM Issue

--- worker/src/engine.ts
+++ worker/src/engine.ts
@@ -1,6 +1,8 @@
 import { AxiosInstance } from "axios";
-
+import {TelemetryEvent} from "../../shared/protocol.js"
 export class LoadEngine {
   private active = false;
   private http: AxiosInstance;
+
+    constructor(private nodeId : string , private onReport :(data :TelemetryEvent ))
 }
