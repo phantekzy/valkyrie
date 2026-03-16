cf8fe77038f56b074b80dc27e4e59561ed4115cf
phantekzy <mainilotfi@gmail.com>
2026-03-16 09:53:43 +0100

    building the TUI

--- controler/src/index.ts
+++ controler/src/index.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
-
+import blessed from "blessed";
 const redis = createClient({ url: "redis://127.0.0.1:6379" });
 const pub = redis.duplicate();
 
 await Promise.all([redis.connect(), pub.connect()]);
+
+const screen = blessed.screen();
