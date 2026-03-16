4c048645d8dc910819ca66f1e0a856840f77ec2a
phantekzy <mainilotfi@gmail.com>
2026-03-16 10:21:14 +0100

    grid and lines

--- controler/src/index.ts
+++ controler/src/index.ts
@@ -1,8 +1,20 @@
 import { createClient } from "redis";
 import blessed from "blessed";
+import contrib from "blessed-contrib";
 const redis = createClient({ url: "redis://127.0.0.1:6379" });
 const pub = redis.duplicate();
 
 await Promise.all([redis.connect(), pub.connect()]);
 
 const screen = blessed.screen();
+const grid = new contrib.grid({ rows: 12, cols: 12, screen });
+const line = grid.set(0, 0, 8, 12, contrib.line, {
+  label: " Cluster Latency P99 (ms) ",
+  style: { line: "yellow" },
+});
+
+screen.render();
+
+screen.key(["q", "C-c"], () => {
+  return process.exit(0);
+});
