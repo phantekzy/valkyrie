2b8c23d577803f7db9c3f022e96bcda1913b2cc6
phantekzy <mainilotfi@gmail.com>
2026-03-16 10:35:38 +0100

    i go to sleep after 17 hours of coding

--- controler/src/index.ts
+++ controler/src/index.ts
@@ -1,13 +1,12 @@
 import { createClient } from "redis";
 import blessed from "blessed";
 import contrib from "blessed-contrib";
+
 const redis = createClient({ url: "redis://127.0.0.1:6379" });
 const pub = redis.duplicate();
-
 await Promise.all([redis.connect(), pub.connect()]);
 
 const screen = blessed.screen();
-
 const grid = new contrib.grid({ rows: 12, cols: 12, screen });
 
 const line = grid.set(0, 0, 8, 12, contrib.line, {
